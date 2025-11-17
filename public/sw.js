f8d6bf151bd7113247007e72546e5a40aa260b36
Thoriq Ichsani <108530206+RikChat@users.noreply.github.com>
2025-11-17 15:22:04 +0700

    Delete public directory

--- public/sw.js
+++
@@ -1,7 +0,0 @@
-// Basic empty service worker (placeholder). For advanced caching, replace with Workbox or custom logic.
-self.addEventListener('install', function(event){
-  self.skipWaiting();
-});
-self.addEventListener('activate', function(event){
-  self.clients.claim();
-});
