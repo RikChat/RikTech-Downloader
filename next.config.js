e7a5499123fa9efae3c05dd76fa9f200c6e0ade0
Thoriq Ichsani <108530206+RikChat@users.noreply.github.com>
2025-11-17 14:20:41 +0700

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: { domains: ['i.ytimg.com','pbs.twimg.com','instagram.fxyz1-1.fna.fbcdn.net'] }
+}
+module.exports = nextConfig
